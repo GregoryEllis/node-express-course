245d898135e2309fd3d79ba5140eb3f22749f2be
GregoryEllis <gregorye917@gmail.com>
2022-06-20 21:55:12 -0400

    new commit

--- 01-node-tutorial/answers/myApp.js
+++ 01-node-tutorial/answers/myApp.js
@@ -1,2 +1,10 @@
 // myApp
 
+// With dependencies you have to install them first before you are to use them.
+
+const _ = require('lodash');
+
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items);
+console.log(newItems);
+console.log('hello world')
